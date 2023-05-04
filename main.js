f87f3800c5d92cefa509bba96cf0c4fefaebedd2
Nicolas Paula <nicolaspaula185@gmail.com>
2023-05-04 15:59:23 -0300

    correção da logica

--- main.js
+++ main.js
@@ -1,88 +1,98 @@
-const inputProduto = document.querySelector('#name');
-const botao = document.querySelector('.btn')
-const resultado = document.querySelector('.resultado')
-const botaoLimpar = document.querySelector('.del')
-const produtosJSON = JSON.parse(localStorage.getItem('produtos'));
+const nomeProduto = document.querySelector('#name');
+const btnCadastrar = document.querySelector('.btn-cadastrar');
+const resultado = document.querySelector('#saida');
+const botaoLimpar = document.querySelector('.btn-excluir');
+let produtos = [];
 
-const produtos = [];
 
-// Verifica se o usuário já tinha salvado dados antes, se tiver, ele vai carregá-los antes de colocar os novos
-if (localStorage.hasOwnProperty('produtos')) {
-  produtos.push(...produtosJSON);
-  produtos.forEach(produto => criaProduto(produto))
+function carregaProdutosDoLocalStorage() {
+	if (localStorage.hasOwnProperty('produtos')) {
+		produtos = JSON.parse(localStorage.getItem('produtos'));
+		produtos.forEach(produto => criaProdutoHTML(produto));
+	}
 }
 
 
-// salva no localStorage e cria o elemento html
-function salvaProduto() {
-
-  botao.addEventListener('click', function (e) {
-    e.preventDefault();
-
-    const inputProdutoValue = inputProduto.value;
-    produtos.push(inputProdutoValue);
-
-    localStorage.setItem('produtos', JSON.stringify(produtos));
-    console.log(produtos);
-    criaProduto(inputProdutoValue);
-    inputProduto.focus()
-    inputProduto.value = '';
-  });
-}
-
-// cria o produto html
-function criaProduto(textoInput) {
-  const p = document.createElement('p');
-  const span = document.createElement('span');
-  p.innerHTML = textoInput;
-  resultado.appendChild(span);
-  span.appendChild(p);
-  apagar(p, span)
+function salvaProduto(produto) {
+	produtos.push(produto);
+	localStorage.setItem('produtos', JSON.stringify(produtos));
 }
 
 
-// apagar tanto do localStorage quanto os elementos html
-function apagar(p, span) {
-  p.innerHTML += ' ';
-  const botaoApagar = document.createElement('button');
-  botaoApagar.innerHTML = 'Apagar'
-  botaoApagar.setAttribute('class', 'botao-apagar')
-  span.appendChild(botaoApagar);
-
-  botaoApagar.addEventListener('click', function (e) {
-    e.preventDefault();
-    const indexP = produtos.indexOf(p.innerHTML.replace(' Apagar', ''));
-    if (indexP > -1) {
-      produtos.splice(indexP, 1);
-      localStorage.setItem('produtos', JSON.stringify(produtos));
-    }
-    span.remove();
-
-    // verifica se o elemento html é igual o do localStorage, se for ele deleta do localstorage 
-    //
-    for (let produto of produtos) {
-      if (produto === p.innerText.trim()) {
-        let indexProduto = produtos.indexOf(produto)
-        produtos.splice(indexProduto, 1)
-        localStorage.setItem('produtos', JSON.stringify(produtos));
-      }
-    }
-  });
+function criaProdutoHTML(produto) {
+	const p = document.createElement('p');
+	const botaoApagar = document.createElement('button');
+	botaoApagar.innerHTML = 'Apagar';
+	botaoApagar.classList.add('botao-apagar');
+	p.innerHTML = `${produto} ${botaoApagar.outerHTML}`;
+	resultado.appendChild(p);
 }
 
+function removeProduto(produtoHTML) {
+	const index = Array.from(resultado.children).indexOf(produtoHTML);
+	if (index > -1) {
+		produtos.splice(index, 1);
+		localStorage.setItem('produtos', JSON.stringify(produtos));
+	}
+	produtoHTML.remove();
+}
 
-// limpa o localStorage
+// function removeProduto(produtoHTML) {
+// 	const texto = produtoHTML.textContent.trim().replace('Apagar', '');
+// 	const index = produtos.indexOf(texto);
+// 	if (index > -1) {
+// 		produtos.splice(index, 1);
+// 		localStorage.setItem('produtos', JSON.stringify(produtos));
+// 	}
+// 	produtoHTML.remove();
+
+// // 	for (let produto of produtos) {
+// // 		if (produto === produtoHTML.innerText.trim()) {
+// // 				let indexProduto = produtos.indexOf(produto)
+// // 				produtos.splice(indexProduto, 1)
+// // 				localStorage.setItem('produtos', JSON.stringify(produtos));
+// // 				alert(produto)
+// // 		}
+
+// // }
+// }
+
+
 function limparTudo() {
-  localStorage.removeItem('produtos');
-  produtos = [];
-  resultado.innerHTML = '';
+	localStorage.removeItem('produtos');
+	produtos = [];
+	resultado.innerHTML = '';
 }
 
 
-// evento pra limpar o localStorage
-botaoLimpar.addEventListener('click', function (e) {
-  e.preventDefault();
-  limparTudo();
+
+
+btnCadastrar.addEventListener('click', function (e) {
+	e.preventDefault();
+	const nome = nomeProduto.value.trim();
+	if (nome !== '') {
+		salvaProduto(nome);
+		criaProdutoHTML(nome);
+		nomeProduto.focus();
+	} else {
+		alert('Preencha os dados');
+	}
+	nomeProduto.value = '';
 });
 
-salvaProduto();
+
+resultado.addEventListener('click', function (e) {
+	const el = e.target;
+	if (el.classList.contains('botao-apagar')) {
+		const p = el.parentElement;
+		removeProduto(p);
+	}
+});
+
+
+botaoLimpar.addEventListener('click', function (e) {
+	e.preventDefault();
+	limparTudo();
+});
+
+carregaProdutosDoLocalStorage();
---
+++ readme.md
@@ -0,0 +1,20 @@
+# Software de controle de estoque usando apenas JavaScript, HTML e CSS
+
+Este projeto é um gerenciador de estoque que usa HTML, CSS e Javascript para criar uma interface web simples e funcional. O objetivo é permitir que o usuário cadastre, consulte, edite e exclua produtos do estoque, bem como visualize o valor total do estoque.
+
+## Funcionalidades
+
+- O projeto possui as seguintes funcionalidades:
+  - Cadastro de produtos: o usuário pode inserir os dados de um produto (nome, preço, quantidade e descrição) e salvá-lo no estoque.
+  - Consulta de produtos: o usuário pode pesquisar um produto pelo nome ou pelo código e ver os seus dados na tela.
+  - Edição de produtos: o usuário pode alterar os dados de um produto já cadastrado e atualizá-lo no estoque.
+  - Exclusão de produtos: o usuário pode remover um produto do estoque, desde que ele não tenha nenhuma dependência com outros produtos.
+  - Valor total do estoque: o usuário pode ver o valor total do estoque, que é a soma dos preços de todos os produtos multiplicados pelas suas quantidades.
+
+## Tecnologias
+
+- O projeto usa as seguintes tecnologias:
+  - HTML: para estruturar o conteúdo da página web.
+  - CSS: para estilizar a aparência da página web.
+  - Javascript: para adicionar interatividade e lógica à página web.
+  - LocalStorage: para armazenar os dados dos produtos no navegador do usuário.
